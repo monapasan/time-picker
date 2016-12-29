d8dd7d137a31d18557ffa43251b19bf0a5a96802
monapasan <oleg.yarin@gmail.com>
2016-12-29 16:37:49 +0100

    Remove React.create class syntax, use babel to build

--- src/Combobox.jsx
+++ src/Combobox.jsx
@@ -1,129 +1,121 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import Select from './Select';
 
 const formatOption = (option, disabledOptions) => {
-  let value = `${option}`;
-  if (option < 10) {
-    value = `0${option}`;
-  }
+  const value = option < 10 ?  `0${option}` : `${option}`
 
-  let disabled = false;
-  if (disabledOptions && disabledOptions.indexOf(option) >= 0) {
-    disabled = true;
-  }
+  const disabled = disabledOptions && disabledOptions.indexOf(option) >= 0
+      ? true
+      : false
+  return { value, disabled }
+}
 
-  return {
-    value,
-    disabled,
-  };
-};
+class Combobox extends Component {
+    static propTypes: {
+        format: PropTypes.string,
+        defaultOpenValue: PropTypes.object,
+        prefixCls: PropTypes.string,
+        value: PropTypes.object,
+        onChange: PropTypes.func,
+        showHour: PropTypes.bool,
+        showMinute: PropTypes.bool,
+        showSecond: PropTypes.bool,
+        hourOptions: PropTypes.array,
+        minuteOptions: PropTypes.array,
+        secondOptions: PropTypes.array,
+        disabledHours: PropTypes.func,
+        disabledMinutes: PropTypes.func,
+        disabledSeconds: PropTypes.func,
+        onCurrentSelectPanelChange: PropTypes.func,
+    };
 
-const Combobox = React.createClass({
-  propTypes: {
-    format: PropTypes.string,
-    defaultOpenValue: PropTypes.object,
-    prefixCls: PropTypes.string,
-    value: PropTypes.object,
-    onChange: PropTypes.func,
-    showHour: PropTypes.bool,
-    showMinute: PropTypes.bool,
-    showSecond: PropTypes.bool,
-    hourOptions: PropTypes.array,
-    minuteOptions: PropTypes.array,
-    secondOptions: PropTypes.array,
-    disabledHours: PropTypes.func,
-    disabledMinutes: PropTypes.func,
-    disabledSeconds: PropTypes.func,
-    onCurrentSelectPanelChange: PropTypes.func,
-  },
-
-  onItemChange(type, itemValue) {
-    const { onChange, defaultOpenValue } = this.props;
-    const value = (this.props.value || defaultOpenValue).clone();
-    if (type === 'hour') {
-      value.hour(itemValue);
-    } else if (type === 'minute') {
-      value.minute(itemValue);
-    } else {
-      value.second(itemValue);
+    onItemChange(type, itemValue) {
+        const { onChange, defaultOpenValue } = this.props
+        const value = (this.props.value || defaultOpenValue).clone()
+        if (type === 'hour') {
+            value.hour(itemValue)
+        } else if (type === 'minute') {
+            value.minute(itemValue)
+        } else {
+            value.second(itemValue)
+        }
+        onChange(value)
     }
-    onChange(value);
-  },
 
-  onEnterSelectPanel(range) {
-    this.props.onCurrentSelectPanelChange(range);
-  },
-
-  getHourSelect(hour) {
-    const { prefixCls, hourOptions, disabledHours, showHour } = this.props;
-    if (!showHour) {
-      return null;
+    onEnterSelectPanel(range) {
+        this.props.onCurrentSelectPanelChange(range);
     }
-    const disabledOptions = disabledHours();
 
-    return (
-      <Select
-        prefixCls={prefixCls}
-        options={hourOptions.map(option => formatOption(option, disabledOptions))}
-        selectedIndex={hourOptions.indexOf(hour)}
-        type="hour"
-        onSelect={this.onItemChange}
-        onMouseEnter={this.onEnterSelectPanel.bind(this, 'hour')}
-      />
-    );
-  },
+    getHourSelect(hour) {
+        const { prefixCls, hourOptions, disabledHours, showHour } = this.props
+        if (!showHour) {
+            return null
+        }
+        const disabledOptions = disabledHours()
 
-  getMinuteSelect(minute) {
-    const { prefixCls, minuteOptions, disabledMinutes, defaultOpenValue, showMinute } = this.props;
-    if (!showMinute) {
-      return null;
+        return (
+            <Select
+                prefixCls={prefixCls}
+                options={hourOptions.map(option => formatOption(option, disabledOptions))}
+                selectedIndex={hourOptions.indexOf(hour)}
+                type="hour"
+                onSelect={this.onItemChange}
+                onMouseEnter={this.onEnterSelectPanel.bind(this, 'hour')}
+            />
+        )
     }
-    const value = this.props.value || defaultOpenValue;
-    const disabledOptions = disabledMinutes(value.hour());
 
-    return (
-      <Select
-        prefixCls={prefixCls}
-        options={minuteOptions.map(option => formatOption(option, disabledOptions))}
-        selectedIndex={minuteOptions.indexOf(minute)}
-        type="minute"
-        onSelect={this.onItemChange}
-        onMouseEnter={this.onEnterSelectPanel.bind(this, 'minute')}
-      />
-    );
-  },
+    getMinuteSelect(minute) {
+        const { prefixCls, minuteOptions, disabledMinutes, defaultOpenValue, showMinute } = this.props
+        if (!showMinute) {
+            return null
+        }
+        const value = this.props.value || defaultOpenValue
+        const disabledOptions = disabledMinutes(value.hour())
 
-  getSecondSelect(second) {
-    const { prefixCls, secondOptions, disabledSeconds, showSecond, defaultOpenValue } = this.props;
-    if (!showSecond) {
-      return null;
+        return (
+            <Select
+                prefixCls={prefixCls}
+                options={minuteOptions.map(option => formatOption(option, disabledOptions))}
+                selectedIndex={minuteOptions.indexOf(minute)}
+                type="minute"
+                onSelect={this.onItemChange}
+                onMouseEnter={this.onEnterSelectPanel.bind(this, 'minute')}
+            />
+        )
     }
-    const value = this.props.value || defaultOpenValue;
-    const disabledOptions = disabledSeconds(value.hour(), value.minute());
 
-    return (
-      <Select
-        prefixCls={prefixCls}
-        options={secondOptions.map(option => formatOption(option, disabledOptions))}
-        selectedIndex={secondOptions.indexOf(second)}
-        type="second"
-        onSelect={this.onItemChange}
-        onMouseEnter={this.onEnterSelectPanel.bind(this, 'second')}
-      />
-    );
-  },
+    getSecondSelect(second) {
+        const { prefixCls, secondOptions, disabledSeconds, showSecond, defaultOpenValue } = this.props
+        if (!showSecond) {
+            return null
+        }
+        const value = this.props.value || defaultOpenValue
+        const disabledOptions = disabledSeconds(value.hour(), value.minute())
 
-  render() {
-    const { prefixCls, defaultOpenValue } = this.props;
-    const value = this.props.value || defaultOpenValue;
-    return (
-      <div className={`${prefixCls}-combobox`}>
-        {this.getHourSelect(value.hour())}
-        {this.getMinuteSelect(value.minute())}
-        {this.getSecondSelect(value.second())}
-      </div>
-    );
-  },
-});
+        return (
+            <Select
+                prefixCls={prefixCls}
+                options={secondOptions.map(option => formatOption(option, disabledOptions))}
+                selectedIndex={secondOptions.indexOf(second)}
+                type="second"
+                onSelect={this.onItemChange}
+                onMouseEnter={this.onEnterSelectPanel.bind(this, 'second')}
+            />
+        )
+    }
 
-export default Combobox;
+    render() {
+        const { prefixCls, defaultOpenValue } = this.props
+        const value = this.props.value || defaultOpenValue
+        return (
+            <div className={`${prefixCls}-combobox`}>
+                {this.getHourSelect(value.hour())}
+                {this.getMinuteSelect(value.minute())}
+                {this.getSecondSelect(value.second())}
+            </div>
+        )
+    }
+}
+
+export default Combobox
--- src/Panel.jsx
+++ src/Panel.jsx
@@ -1,150 +1,152 @@
-import React, { PropTypes } from 'react';
-import Header from './Header';
-import Combobox from './Combobox';
-import moment from 'moment';
-import classNames from 'classnames';
+import React, { PropTypes, Component } from 'react'
+import Header from './Header'
+import Combobox from './Combobox'
+import moment from 'moment'
+import classNames from 'classnames'
 
 function noop() {
 }
 
 function generateOptions(length, disabledOptions, hideDisabledOptions) {
-  const arr = [];
-  for (let value = 0; value < length; value++) {
-    if (!disabledOptions || disabledOptions.indexOf(value) < 0 || !hideDisabledOptions) {
-      arr.push(value);
+    const arr = []
+    for (let value = 0; value < length; value++) {
+        if (
+            !disabledOptions ||
+            disabledOptions.indexOf(value) < 0 ||
+            !hideDisabledOptions
+        ) {
+            arr.push(value)
+        }
     }
-  }
-  return arr;
+    return arr
 }
 
-const Panel = React.createClass({
-  propTypes: {
-    clearText: PropTypes.string,
-    prefixCls: PropTypes.string,
-    className: PropTypes.string,
-    defaultOpenValue: PropTypes.object,
-    value: PropTypes.object,
-    placeholder: PropTypes.string,
-    format: PropTypes.string,
-    disabledHours: PropTypes.func,
-    disabledMinutes: PropTypes.func,
-    disabledSeconds: PropTypes.func,
-    hideDisabledOptions: PropTypes.bool,
-    onChange: PropTypes.func,
-    onEsc: PropTypes.func,
-    allowEmpty: PropTypes.bool,
-    showHour: PropTypes.bool,
-    showMinute: PropTypes.bool,
-    showSecond: PropTypes.bool,
-    onClear: PropTypes.func,
-    addon: PropTypes.func,
-  },
-
-  getDefaultProps() {
-    return {
-      prefixCls: 'rc-time-picker-panel',
-      onChange: noop,
-      onClear: noop,
-      disabledHours: noop,
-      disabledMinutes: noop,
-      disabledSeconds: noop,
-      defaultOpenValue: moment(),
-      addon: noop,
+class Panel extends Component {
+    static propTypes: {
+        clearText: PropTypes.string,
+        prefixCls: PropTypes.string,
+        className: PropTypes.string,
+        defaultOpenValue: PropTypes.object,
+        value: PropTypes.object,
+        placeholder: PropTypes.string,
+        format: PropTypes.string,
+        disabledHours: PropTypes.func,
+        disabledMinutes: PropTypes.func,
+        disabledSeconds: PropTypes.func,
+        hideDisabledOptions: PropTypes.bool,
+        onChange: PropTypes.func,
+        onEsc: PropTypes.func,
+        allowEmpty: PropTypes.bool,
+        showHour: PropTypes.bool,
+        showMinute: PropTypes.bool,
+        showSecond: PropTypes.bool,
+        onClear: PropTypes.func,
+        addon: PropTypes.func,
     };
-  },
 
-  getInitialState() {
-    return {
-      value: this.props.value,
-      selectionRange: [],
-    };
-  },
-
-  componentWillReceiveProps(nextProps) {
-    const value = nextProps.value;
-    if (value) {
-      this.setState({
-        value,
-      });
+    getDefaultProps() {
+        return {
+            prefixCls: 'rc-time-picker-panel',
+            onChange: noop,
+            onClear: noop,
+            disabledHours: noop,
+            disabledMinutes: noop,
+            disabledSeconds: noop,
+            defaultOpenValue: moment(),
+            addon: noop,
+        }
     }
-  },
 
-  onChange(newValue) {
-    this.setState({ value: newValue });
-    this.props.onChange(newValue);
-  },
+    getInitialState() {
+        return {
+            value: this.props.value,
+            selectionRange: [],
+        }
+    }
 
-  onClear() {
-    this.props.onClear();
-  },
+    componentWillReceiveProps(nextProps) {
+        const value = nextProps.value
+        if (value) {
+            this.setState({ value })
+        }
+    }
 
-  onCurrentSelectPanelChange(currentSelectPanel) {
-    this.setState({ currentSelectPanel });
-  },
+    onChange(newValue) {
+        this.setState({ value: newValue })
+        this.props.onChange(newValue)
+    }
 
-  close() {
-    this.props.onEsc();
-  },
+    onClear() {
+        this.props.onClear()
+    }
 
-  render() {
-    const {
-      prefixCls, className, placeholder, disabledHours, disabledMinutes,
-      disabledSeconds, hideDisabledOptions, allowEmpty, showHour, showMinute, showSecond,
-      format, defaultOpenValue, clearText, onEsc, addon,
-    } = this.props;
-    const {
-      value, currentSelectPanel,
-    } = this.state;
-    const disabledHourOptions = disabledHours();
-    const disabledMinuteOptions = disabledMinutes(value ? value.hour() : null);
-    const disabledSecondOptions = disabledSeconds(value ? value.hour() : null,
-      value ? value.minute() : null);
-    const hourOptions = generateOptions(24, disabledHourOptions, hideDisabledOptions);
-    const minuteOptions = generateOptions(60, disabledMinuteOptions, hideDisabledOptions);
-    const secondOptions = generateOptions(60, disabledSecondOptions, hideDisabledOptions);
+    onCurrentSelectPanelChange(currentSelectPanel) {
+        this.setState({ currentSelectPanel })
+    }
 
-    return (
-      <div className={classNames({ [`${prefixCls}-inner`]: true, [className]: !!className })}>
-        <Header
-          clearText={clearText}
-          prefixCls={prefixCls}
-          defaultOpenValue={defaultOpenValue}
-          value={value}
-          currentSelectPanel={currentSelectPanel}
-          onEsc={onEsc}
-          format={format}
-          placeholder={placeholder}
-          hourOptions={hourOptions}
-          minuteOptions={minuteOptions}
-          secondOptions={secondOptions}
-          disabledHours={disabledHours}
-          disabledMinutes={disabledMinutes}
-          disabledSeconds={disabledSeconds}
-          onChange={this.onChange}
-          onClear={this.onClear}
-          allowEmpty={allowEmpty}
-        />
-        <Combobox
-          prefixCls={prefixCls}
-          value={value}
-          defaultOpenValue={defaultOpenValue}
-          format={format}
-          onChange={this.onChange}
-          showHour={showHour}
-          showMinute={showMinute}
-          showSecond={showSecond}
-          hourOptions={hourOptions}
-          minuteOptions={minuteOptions}
-          secondOptions={secondOptions}
-          disabledHours={disabledHours}
-          disabledMinutes={disabledMinutes}
-          disabledSeconds={disabledSeconds}
-          onCurrentSelectPanelChange={this.onCurrentSelectPanelChange}
-        />
-        {addon(this)}
-      </div>
-    );
-  },
-});
+    close() {
+        this.props.onEsc()
+    }
 
-export default Panel;
+    render() {
+        const {
+            prefixCls, className, placeholder, disabledHours, disabledMinutes,
+            disabledSeconds, hideDisabledOptions, allowEmpty, showHour, showMinute,
+            showSecond, format, defaultOpenValue, clearText, onEsc, addon,
+        } = this.props
+
+        const { value, currentSelectPanel, } = this.state
+        const disabledHourOptions = disabledHours()
+        const disabledMinuteOptions = disabledMinutes(value ? value.hour() : null)
+        const disabledSecondOptions = disabledSeconds(
+            value ? value.hour() : null, value ? value.minute() : null
+        )
+        const hourOptions = generateOptions(24, disabledHourOptions, hideDisabledOptions)
+        const minuteOptions = generateOptions(60, disabledMinuteOptions, hideDisabledOptions)
+        const secondOptions = generateOptions(60, disabledSecondOptions, hideDisabledOptions)
+
+        return (
+            <div className={classNames({ [`${prefixCls}-inner`]: true, [className]: !!className })}>
+                <Header
+                  clearText={clearText}
+                  prefixCls={prefixCls}
+                  defaultOpenValue={defaultOpenValue}
+                  value={value}
+                  currentSelectPanel={currentSelectPanel}
+                  onEsc={onEsc}
+                  format={format}
+                  placeholder={placeholder}
+                  hourOptions={hourOptions}
+                  minuteOptions={minuteOptions}
+                  secondOptions={secondOptions}
+                  disabledHours={disabledHours}
+                  disabledMinutes={disabledMinutes}
+                  disabledSeconds={disabledSeconds}
+                  onChange={this.onChange}
+                  onClear={this.onClear}
+                  allowEmpty={allowEmpty}
+                />
+                <Combobox
+                  prefixCls={prefixCls}
+                  value={value}
+                  defaultOpenValue={defaultOpenValue}
+                  format={format}
+                  onChange={this.onChange}
+                  showHour={showHour}
+                  showMinute={showMinute}
+                  showSecond={showSecond}
+                  hourOptions={hourOptions}
+                  minuteOptions={minuteOptions}
+                  secondOptions={secondOptions}
+                  disabledHours={disabledHours}
+                  disabledMinutes={disabledMinutes}
+                  disabledSeconds={disabledSeconds}
+                  onCurrentSelectPanelChange={this.onCurrentSelectPanelChange}
+                />
+                {addon(this)}
+            </div>
+        )
+    }
+}
+
+export default Panel
